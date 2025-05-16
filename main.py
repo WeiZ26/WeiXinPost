a9f1b17354823ac54f3e3df164085e254759121b
WeiZ26 <102684912+WeiZ26@users.noreply.github.com>
2025-05-16 19:10:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from time import localtime
 from requests import get, post
 from datetime import datetime, date
 
-
 # 微信获取token
 def get_access_token():
     # appId
@@ -14,17 +13,18 @@ def get_access_token():
     app_secret = config.app_secret
     post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
                 .format(app_id, app_secret))
-    print(get(post_url).json())
-    access_token = get(post_url).json()['access_token']
-    # print(access_token)
-    return access_token
-
+    try:
+        response = get(post_url)
+        response.raise_for_status()
+        return response.json()['access_token']
+    except Exception as e:
+        print(f"获取access_token失败: {e}")
+        return None
 
 # 获取城市天气
 def get_weather(province, city):
     # 城市id
     city_id = cityinfo.cityInfo[province][city]["AREAID"]
-    # city_id = 101280101
     # 毫秒级时间戳
     t = (int(round(time.time() * 1000)))
     headers = {
@@ -33,86 +33,27 @@ def get_weather(province, city):
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
     url = "http://d1.weather.com.cn/dingzhi/{}.html?_={}".format(city_id, t)
-    response = get(url, headers=headers)
-    response.encoding = "utf-8"
-    response_data = response.text.split(";")[0].split("=")[-1]
-    response_json = eval(response_data)
-    print(response_json)
-    weatherinfo = response_json["weatherinfo"]
-    # 天气
-    weather = weatherinfo["weather"]
-    # 最高气温
-    temp = weatherinfo["temp"]
-    # 最低气温
-    tempn = weatherinfo["tempn"]
-    return weather, temp, tempn
-
-
-# 获取今天是第几周，返回字符串
-def get_Today_Week():
-    y = config.year
-    m = config.month
-    d = config.day
-    startWeek = datetime(y, m, d)
-    today = datetime.today()
-    d_days = today - startWeek
-    trueWeek = (d_days.days // 7) + 1
-    return str(trueWeek)
-
-
-# 获取本周课程
-def get_Week_Classes(w):
-    if w is not None:
-        week_Class = config.classes.get(w)
-    else:
-        week = get_Today_Week()
-        week_Class = config.classes.get(week)
-    return week_Class
-
-
-# 获取今日课程
-def get_Today_Class():
-    year = localtime().tm_year
-    month = localtime().tm_mon
-    day = localtime().tm_mday
-    today = datetime.date(datetime(year=year, month=month, day=day))
-    todayClasses = get_Week_Classes(None)[today.weekday()]
-    return todayClasses
-
-
-# 获取指定星期几的课程
-def get_Class(day):
-    theClasses = get_Week_Classes(None)[day]
-    return theClasses
-
-
-# # 发送本周所有课程，周一的时候发
-# def send_Week_Classes(to_user, access_token, week):
-#     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
-#     theuser = to_user[0]
-#     data = {
-#         "touser": theuser,
-#         "template_id": config.template_id2,
-#         "url": "http://weixin.qq.com/download",
-#         "topcolor": "#FF0000",
-#         "data": {
-#             "weeks": {
-#                 "value": classInfo,
-#                 "color": "#FF8000"
-#             }
-#         }
-#     }
-#     headers = {
-#         'Content-Type': 'application/json',
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-#                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-#     }
-#     response = post(url, headers=headers, json=data)
-#     print(response.text)
-
+    try:
+        response = get(url, headers=headers)
+        response.encoding = "utf-8"
+        response_data = response.text.split(";")[0].split("=")[-1]
+        response_json = eval(response_data)
+        weatherinfo = response_json["weatherinfo"]
+        # 天气
+        weather = weatherinfo["weather"]
+        # 最高气温
+        temp = weatherinfo["temp"]
+        # 最低气温
+        tempn = weatherinfo["tempn"]
+        return weather, temp, tempn
+    except Exception as e:
+        print(f"获取天气信息失败: {e}")
+        return None, None, None
 
 # 发送每日信息
 def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature):
+    if access_token is None:
+        return
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
     year = localtime().tm_year
@@ -121,8 +62,6 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
     today = datetime.date(datetime(year=year, month=month, day=day))
     # 星期几
     week = week_list[today.weekday()]
-    # 开学的第几周
-    weeks = get_Today_Week()
     # 获取在一起的日子的日期格式
     love_year = int(config.love_date.split("-")[0])
     love_month = int(config.love_date.split("-")[1])
@@ -130,210 +69,74 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
     love_date = date(love_year, love_month, love_day)
     # 获取在一起的日期差
     love_days = str(today.__sub__(love_date)).split(" ")[0]
-    # 获取生日的月和日
-    birthday_month = int(config.birthday.split("-")[1])
-    birthday_day = int(config.birthday.split("-")[2])
-    # 今年生日
-    year_date = date(year, birthday_month, birthday_day)
-    # 计算生日年份，如果还没过，按当年减，如果过了需要+1
-    if today > year_date:
-        birth_date = date((year + 1), birthday_month, birthday_day)
-        birth_day = str(birth_date.__sub__(today)).split(" ")[0]
-    elif today == year_date:
-        birth_day = 0
-    else:
-        birth_date = year_date
-        birth_day = str(birth_date.__sub__(today)).split(" ")[0]
-
-    theClass = get_Today_Class()
-    theuser = to_user[0]
-    data = {
-        "touser": theuser,
-        "template_id": config.template_id1,
-        "url": "http://weixin.qq.com/download",
-        "topcolor": "#FF0000",
-        "data": {
-            "weeks": {
-                "value": weeks,
-                "color": "#00FFFF"
-            },
-            "date": {
-                "value": "{} {}".format(today, week),
-                "color": "#00FFFF"
-            },
-            "city": {
-                "value": city_name,
-                "color": "#808A87"
-            },
-            "weather": {
-                "value": weather,
-                "color": "#ED9121"
-            },
-            "min_temperature": {
-                "value": min_temperature,
-                "color": "#00FF00"
-            },
-            "max_temperature": {
-                "value": max_temperature,
-                "color": "#FF6100"
-            },
-            "love_day": {
-                "value": love_days,
-                "color": "#87CEEB"
-            },
-            "birthday": {
-                "value": birth_day,
-                "color": "#FF8000"
-            },
-            "firstClass": {
-                "value": theClass[0],
-                "color": "#FF8000"
-            },
-            "secondClass": {
-                "value": theClass[1],
-                "color": "#FF8000"
-            },
-            "thirdClass": {
-                "value": theClass[2],
-                "color": "#FF8000"
-            },
-            "fourthClass": {
-                "value": theClass[3],
-                "color": "#FF8000"
-            },
-            "fifthClass": {
-                "value": theClass[4],
-                "color": "#FF8000"
-            },
-            "sixthClass": {
-                "value": theClass[5],
-                "color": "#FF8000"
-            }
-        }
-    }
+    # 定义headers变量
     headers = {
-        'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
-    response = post(url, headers=headers, json=data)
-    print(response.text)
-
-
-# 发送课程消息
-def send_Class_Message(to_user, access_token, classInfo):
-    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
-    theuser = to_user[0]
-    data = {
-        "touser": theuser,
-        "template_id": config.template_id2,
-        "url": "http://weixin.qq.com/download",
-        "topcolor": "#FF0000",
-        "data": {
-            "classInfo": {
-                "value": classInfo,
-                "color": "#FF8000"
-            }
-        }
-    }
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    response = post(url, headers=headers, json=data)
-    print(response.text)
-
-
-# 发送晚安心语及第二天课程
-def send_Good_Night(to_user, access_token):
-    week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    # 获取天行数据晚安心语
-    txUrl = "http://api.tianapi.com/wanan/index"
+    # 获取天行数据每日一句
+    # 注意：这一行和下面的代码应该与上面的代码保持相同的缩进级别
+    txUrl = "http://apis.tianapi.com/saylove/index"  # 确保这一行与上面的代码对齐
     key = config.good_Night_Key
     pre_data = {"key": key}
-    # param = json.dumps((pre_data))
-    r = post(txUrl, params=pre_data, headers=headers)
-    print("r:", r.text)
-    good_Night = r.json()["newslist"][0]["content"]
-    # good_Night = "晚安"
-    # 获取第二天课表
-    year = localtime().tm_year
-    month = localtime().tm_mon
-    day = localtime().tm_mday
-    today = datetime.date(datetime(year=year, month=month, day=day))
-    weekClasses = get_Week_Classes(None)
-    week = week_list[(today.weekday() + 1) % 7]
-    theClass = []
-    if (today.weekday() + 1) % 7 == 0:
-        weekClasses = get_Week_Classes(get_Today_Week())
-        theClass = weekClasses[0]
-    else:
-        theClass = weekClasses[today.weekday() + 1]
-
-    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
-    theuser = to_user[0]
-    data = {
-        "touser": theuser,
-        "template_id": config.template_id3,
-        "url": "http://weixin.qq.com/download",
-        "topcolor": "#FF0000",
-        "data": {
-            "goodNight": {
-                "value": good_Night,
-                "color": "#87CEEB"
-            },
-            "week": {
-                "value": week,
-                "color": "#00FFFF"
-            },
-            "firstClass": {
-                "value": theClass[0],
-                "color": "#FF8000"
-            },
-            "secondClass": {
-                "value": theClass[1],
-                "color": "#FF8000"
-            },
-            "thirdClass": {
-                "value": theClass[2],
-                "color": "#FF8000"
-            },
-            "fourthClass": {
-                "value": theClass[3],
-                "color": "#FF8000"
-            },
-            "fifthClass": {
-                "value": theClass[4],
-                "color": "#FF8000"
-            },
-            "sixthClass": {
-                "value": theClass[5],
-                "color": "#FF8000"
+    try:
+        r = post(txUrl, params=pre_data, headers=headers)
+        r.raise_for_status()
+        data = r.json()
+        if "result" in data and "content" in data["result"]:
+            raw_sentence = data["result"]["content"]
+            good_Night = f"🌞 {raw_sentence}"  # 拼接表情
+        else:
+            print(f"API响应格式异常: {data}")
+            good_Night = "今日问候语获取失败"
+        theuser = to_user[0]
+        data = {
+            "touser": theuser,
+            "template_id": config.template_id1,
+            "url": "http://weixin.qq.com/download",
+            "topcolor": "#FF0000",
+            "data": {
+                "date": {
+                    "value": "{} {}".format(today, week),
+                    "color":"#173177"
+                },
+                "city": {
+                    "value": city_name,
+                    "color":"#173177"
+                },
+                "weather": {
+                    "value": weather,
+                    "color":"#173177"
+                },
+                "min_temperature": {
+                    "value": min_temperature,
+                    "color":"#173177"
+                },
+                "max_temperature": {
+                    "value": max_temperature,
+                    "color":"#173177"
+                },
+                "love_day": {
+                    "value": love_days,
+                    "color":"#173177"
+                },
+                "goodNight": {
+                    "value": good_Night,
+                    "color":"#173177"
+                }
             }
         }
-    }
-    response = post(url, headers=headers, json=data)
-    print(response.text)
+        response = post(url, headers=headers, json=data)
+        response.raise_for_status()
+        print("每日信息推送成功")
+    except Exception as e:
+        print(f"每日信息推送失败: {e}")
 
-
-# 计算时间间隔
-def calculate_Time_Difference(t1, t2):
-    h1 = int(t1[0:2])
-    h2 = int(t2[0:2])
-    m1 = int(t1[3:5])
-    m2 = int(t2[3:5])
-    s1 = int(t1[6:8])
-    s2 = int(t2[6:8])
-    d1 = datetime(2022, 1, 1, h1, m1, s1)
-    d2 = datetime(2022, 1, 1, h2, m2, s2)
-    return (d1 - d2).seconds
-
+# 计算时间差（秒）
+def calculate_Time_Difference(time1, time2):
+    t1 = datetime.strptime(time1, '%H:%M:%S')
+    t2 = datetime.strptime(time2, '%H:%M:%S')
+    return (t1 - t2).total_seconds()
 
 if __name__ == '__main__':
     # 获取accessToken
@@ -346,54 +149,5 @@ if __name__ == '__main__':
     province, city = config.province, config.city
     weather, max_temperature, min_temperature = get_weather(province, city)
     isPost = False
-    # 公众号推送消息
-    if datetime.now().strftime('%H:%M:%S') < config.post_Time:
-        send_message(user, accessToken, city, weather, max_temperature, min_temperature)
-        isPost = True
-    # 课程提醒推送
-    todayClasses = get_Today_Class()
-    time_table = config.time_table
-    for i in range(len(time_table)):
-        if isPost:
-            break
-        reminderTime = time_table[i]
-        while True:
-            nowTime = datetime.now().strftime('%H:%M:%S')
-            print("当前时间:", nowTime)
-            if reminderTime == nowTime:
-                if len(todayClasses[i]) != 0:
-                    classInfo = "课程信息: " + todayClasses[i] + "\n" + "上课时间: " + config.course_Time[i] + "\n"
-                    print(classInfo)
-                    send_Class_Message(user, accessToken, classInfo)
-                    print("课程信息推送成功！")
-                isPost = True
-                break
-            elif reminderTime < nowTime:
-                break
-            # 通过睡眠定时
-            defference = calculate_Time_Difference(reminderTime, nowTime) - 3
-            print("课程推送时间差：", defference, "秒")
-            if defference > 0:
-                print("开始睡眠: 等待推送第", i + 1, "讲课")
-                time.sleep(defference)
-                print("结束睡眠")
-    while True:
-        goodNightTime = config.good_Night_Time
-        nowTime = datetime.now().strftime('%H:%M:%S')
-        if goodNightTime == nowTime:
-            # 发送晚安心语
-            send_Good_Night(user, accessToken)
-            print("晚安心语推送成功！")
-            break
-        elif goodNightTime < nowTime:
-            print("当前时间已过晚安心语推送设置的时间！")
-            break
-        elif calculate_Time_Difference(goodNightTime, nowTime) > 120:
-            break
-        # 通过睡眠定时
-        defference = calculate_Time_Difference(goodNightTime, nowTime) - 3
-        print("晚安心语推送时间差：", defference, "秒")
-        if defference > 0:
-            print("开始睡眠:等待推送晚安心语")
-            time.sleep(defference)
-            print("结束睡眠")
+    send_message(user, accessToken, city, weather, max_temperature, min_temperature)
+    isPost = True
